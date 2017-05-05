eb8bc41a6753b9190bef7ae62c3a543ecb33f40c
RutujaWanjari <rutuja.wanjari2@gmail.com>
2017-05-06 00:16:11 +0530

    Derive color

---
+++ closestcolorname.py
@@ -0,0 +1,32 @@
+# This is a script written to derive closest matching color of image if actual color is not derivable.
+import webcolors
+
+
+def closest_colour(requested_colour):
+    """
+    Calculate closest matching color.
+    @param requested_colour: 
+    @return: 
+    """
+    min_colours = {}
+    for key, name in webcolors.css3_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+
+def get_colour_name(requested_colour):
+    """
+    Get color name
+    @param requested_colour: 
+    @return: 
+    """
+    try:
+        closest_name = actual_name = webcolors.rgb_to_name(requested_colour)
+    except ValueError:
+        closest_name = closest_colour(requested_colour)
+        actual_name = None
+    return actual_name, closest_name
--- getcolor.py
+++ getcolor.py
@@ -1,39 +1,66 @@
+# This is the main script to run.
+# This program demonstrates how a color is identified from an image.
+
 from PIL import Image
-
-im = Image.open("red.jpg")
-
-img = im.load()
-size = im.size
-print(size)
-
-# information = dict()
-# information = im.info()
-#
-# print(information)
+import closestcolorname
 
 
-# from PIL import Image
-#
-#
-# def compute_average_image_color(img):
-#     width, height = img.size
-#
-#     r_total = 0
-#     g_total = 0
-#     b_total = 0
-#
-#     count = 0
-#     for x in range(0, width):
-#         for y in range(0, height):
-#             r, g, b = img.getpixel((x, y))
-#             r_total += r
-#             g_total += g
-#             b_total += b
-#             count += 1
-#
-#     return r_total/count, g_total/count, b_total/count
-#
-# img = Image.open('red.jpg')
-# # img = img.resize((50,50))  # Small optimization
-# average_color = compute_average_image_color(img)
-# print(average_color)
+def avg_img_color(img):
+    """
+    Calculate average of individual red, green and blue colors.
+    @param img: Actual RGB image
+    @return int, int, int: Three average integer values of red, green and blue pixels respectively.
+    """
+    width, height = img.size
+    r_total = 0
+    g_total = 0
+    b_total = 0
+    count = 0
+
+    # Compute total count of red, green and blue color pixel.
+    for x in range(0, width):
+        for y in range(0, height):
+            r, g, b = img.getpixel((x, y))
+            r_total += r
+            g_total += g
+            b_total += b
+            count += 1
+
+    return r_total/count, g_total/count, b_total/count
+
+
+def closest_color(avg_color):
+    """
+    To find actual color name. If actual color name is unknown then derive closest one. 
+    @param avg_color: 
+    @return actual_color_name:
+    @return closest_color_name:
+    """
+    r = int(avg_color[0])
+    g = int(avg_color[1])
+    b = int(avg_color[2])
+    requested_color = (r, g, b)
+    print(requested_color)
+    actual_color_name, closest_color_name = closestcolorname.get_colour_name(requested_color)
+
+    return actual_color_name, closest_color_name
+
+
+def commute_result(img):
+    """
+    Converting image of any form to RGB image and print result.
+    @param img: 
+    """
+    img = img.convert('RGB')
+    avg_color = avg_img_color(img)
+    print("Actual and Closest color name are ", closest_color(avg_color))
+
+
+# The redball.jpg image has a red colored ball with white background. Hence, closest color name is derived as
+# lightcoral. light coral has approx 94% red, 50% green and 50% blue.
+img = Image.open("redball.jpg")
+commute_result(img)
+
+# While the red.png is a fully red colored image. Hence, red color is derived.
+img = Image.open("red.png")
+commute_result(img)
